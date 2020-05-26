531cbfa7ae587d30afc1835427aad5618426f047
Lyss <lyssfitz@outlook.com>
2020-05-26 10:44:02 +1000

    Add DogDetails component and styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import DogList from './DogList';
+import DogDetails from './DogDetails';
 
 import whiskey from './images/whiskey.jpg';
 import tubby from './images/tubby.jpg';
@@ -45,7 +46,17 @@ class App extends Component {
 		],
 	};
 	render() {
-		return <Route path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />;
+		const getDog = (props) => {
+			let name = props.match.params.name;
+			let currentDog = this.props.dogs.find((dog) => dog.name.toLowerCase() === name.toLowerCase());
+			return <DogDetails {...props} dog={currentDog} />;
+		};
+		return (
+			<Switch>
+				<Route exact path="/dogs" render={() => <DogList dogs={this.props.dogs} />} />
+				<Route exact path="/dogs/:name" render={getDog} />
+			</Switch>
+		);
 	}
 }
 
